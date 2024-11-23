eea96094920204355882b8f86afac6ec6b1dc9c6
wannnhi <nanhi@kakao.com>
2024-11-24 01:28:51 +0900

    feat : stats

---
+++ Assets/01_Scripts/Wan/UI/HeroStats.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using TMPro;
+public class HeroStats : MonoBehaviour
+{
+    public EnityStatus status;
+
+    private TMP_Text _upgraded;
+    private TMP_Text _stat;
+    private TMP_Text _sellPrice;
+    private TMP_Text _upgradePrice;
+
+    private Transform _tab;
+
+    private void Awake()
+    {
+        _upgraded = transform.Find("Upgrade").GetComponent<TMP_Text>();
+        _stat = transform.Find("Stat").GetComponent<TMP_Text>();
+        _tab = transform.Find("Tab").GetComponent<Transform>();
+        _sellPrice = _tab.Find("Sell").Find("SellPrice").GetComponent<TMP_Text>();
+        _upgradePrice = _tab.Find("Upgrade").Find("UpgradePrice").GetComponent<TMP_Text>();
+    }
+
+    private void Start()
+    {
+        Initialize();
+    }
+
+    public void Initialize()
+    {
+        float upgradeMultiplier = 1f + (0.3f * status.UpgradeLevel);
+        float speedMultiplier = 1f + (0.1f * status.UpgradeLevel);
+
+        float upgradedAtkDamage = status.atkDamage * upgradeMultiplier;
+        float upgradedSkillDamage = status.skillDamage * upgradeMultiplier;
+        float upgradedAttackSpeed = status.atkDamage * upgradeMultiplier;
+        float upgradedMoveSpeed = status.moveSpeed * speedMultiplier;
+        float upgradedSkillCooldown = status.skilCoolDown * speedMultiplier;
+
+        if (status.skilCoolDown == 0)
+        {
+            _stat.SetText($"공격 {upgradedAtkDamage:F1} 속도 {upgradedMoveSpeed:F1}\n체력 {"pp"} 공속 {upgradedAttackSpeed:F1}");
+            _sellPrice.SetText($"${status.SellPrice}");
+        }
+        else
+        {
+            _stat.SetText($"공격 {upgradedAtkDamage:F1} 속도 {upgradedMoveSpeed:F1}\n체력 {"pp"} 공속 {upgradedAttackSpeed:F1}\n스킬 공격력 {upgradedSkillDamage:F1}\n스킬 쿨타임 {upgradedSkillCooldown:F1}");
+        }
+    }
+
+}
