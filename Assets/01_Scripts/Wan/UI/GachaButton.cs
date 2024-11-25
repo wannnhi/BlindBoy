82babecc229105c5106d7fc44503af1c65de1dd9
wannnhi <nanhi@gmail.com>
2024-11-25 10:14:26 +0900

    feat : Gacha

---
+++ Assets/01_Scripts/Wan/UI/GachaButton.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Playables;
+
+public class GachaButton : MonoBehaviour
+{
+    [SerializeField] private List<EnityStatus> enities;
+    [SerializeField] private PlayableDirector timeline;
+
+    public void GachaStart()
+    {
+
+    }
+}
